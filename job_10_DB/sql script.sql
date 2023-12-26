23b8471e5b1f8829a554022f0d9d1633f5c6dd39
julienRactM <julien.ract@sfr.fr>
2023-12-26 12:58:34 +0100

    work week

---
+++ job_10_DB/sql script.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Country (
+    Country VARCHAR(50),
+    Coal FLOAT,
+    Gas FLOAT,
+    Oil FLOAT,
+    Hydro FLOAT,
+    Renewable FLOAT,
+    Nuclear FLOAT
+);
+CREATE TABLE world (
+    Region VARCHAR(50),
+    Coal FLOAT,
+    Gas FLOAT,
+    Oil FLOAT,
+    Hydro FLOAT,
+    Renewable FLOAT,
+    Nuclear FLOAT
+);
