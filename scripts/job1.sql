9e793a01faacb5f9b34a4b0623d249ea7efef4f5
julienRactM <julien.ract@sfr.fr>
2024-01-07 13:43:42 +0100

    final cleanup

--- scripts/job1.sql
+++ scripts/job1.sql
@@ -1,18 +1,20 @@
 /* DB crée en SQL*/
 
--- 1.
+-- 1. Modifiez la requête ci-dessus afin d’afficher la population de “Germany”.
 SELECT *
 FROM world
 WHERE TRIM(Country) = 'Germany'
 
 
--- 2.
+-- 2. Modifiez la requête ci-dessus afin d’afficher le nom et la population des pays
+-- “Sweden”, “Norway” et “Denmark”.
 SELECT *
 FROM world
 WHERE TRIM(Country) IN ('Sweden', 'Norway', 'Denmark');
 
 
--- 3.
+-- 3. Créez une requête permettant d’afficher les pays dont la superficie est
+-- supérieure à 200 000 mais inférieure à 300 000.
 
 SELECT *
 FROM world
